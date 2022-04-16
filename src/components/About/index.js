6e4b9609b455922658def621dffc761bf31fddf4
Mike Gordon <vprmatrix55@gmail.com>
2022-04-15 20:14:31 -0700

    added animation for about and styling for portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,17 +3,20 @@ import avatar from '../../assets/images/profile3.jpg'
 export default function About() {
     return (
         <div className='about-container'>
-            <h1 className='title'>About Me</h1>
-            <img className="profile-pic" src={avatar} alt="Mike Gordon with lobsters" />
-            <p className='about-text'>
-                Hi, I'm Mike
-
-                I'm a driven person by nature and have a strong work ethic. I have high expectations for myself when it comes to the quality of my work, which translates to an amazing finished product for you!
-
-                Although I do like to code websites from scratch, my real passion is to bring modernity to existing websites through interesting interactive features.
-
-                Check out some of my work below, and please don't hesitate to reach out if you have any questions or want to get your project started
-            </p>
+            <h1 className='about-title'>About Me</h1>
+            <div className='animation-box'>
+                <div className='pic-animation'>
+                    <img className="profile-pic" src={avatar} alt="Mike Gordon with lobsters" />
+                </div>
+                <div className='about-animation'>
+                    <p className='about-text'>
+                        Hi, I'm Mike
+                        I'm a driven person by nature and have a strong work ethic. I have high expectations for myself when it comes to the quality of my work, which translates to an amazing finished product for you!
+                        Although I do like to code websites from scratch, my real passion is to bring modernity to existing websites through interesting interactive features.
+                        Check out some of my work in the Portfolio tab, and please don't hesitate to reach out if you have any questions or want to get your project started
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
